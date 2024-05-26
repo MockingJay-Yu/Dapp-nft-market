84298bd94cb77466ffcabaf85aadef1adde53b5f
MockingJay-Yu <yudengke1016@gmail.com>
2024-05-26 18:51:10 +0800

    增加了部署脚本

--- nft-market-hardhat/script/NFTM-deploy.js
+++ nft-market-hardhat/script/NFTM-deploy.js
@@ -1,7 +1,17 @@
 const hre = require("hardhat");
 
 async function deploy() {
-  const contractFactory = await hre.ethers.getContractFactory("NFTM");
-  const contract = await contractFactory.deploy();
+  const FUSDTFactory = await hre.ethers.getContractFactory("FUSDT");
+  const FUSDT = await FUSDTFactory.deploy();
+  console.log(FUSDT.target);
+
+  const NFTMFactory = await hre.ethers.getContractFactory("NFTM");
+  const NFTM = await NFTMFactory.deploy();
+  console.log(NFTM.target);
+
+  const NftMarketFactory = await hre.ethers.getContractFactory("NftMarket");
+  const NftMarket = await NftMarketFactory.deploy(FUSDT.target, NFTM.target);
+  console.log(NftMarket.target);
 }
+
 deploy().then((result) => {});
